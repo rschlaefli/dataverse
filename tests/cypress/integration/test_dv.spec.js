b7d63c5134db36f31e2352449c688a5e7f392935
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-05-31 20:00:36 +0200

    Add a case for new dataset creation

--- tests/cypress/integration/test_dv.spec.js
+++ tests/cypress/integration/test_dv.spec.js
@@ -2,22 +2,22 @@ import { login } from '../util'
 
 describe('Dataverse', function() {
   before(function() {
-    login(cy, () => {
+    // login(cy, () => {
       // remove any existing dataverse with the test identifier
       // conditional testing should only be used in very rare (synchronous cases)
       // https://docs.cypress.io/guides/core-concepts/conditional-testing.html#Element-existence
-      cy.visit('/dataverse/testDv', { failOnStatusCode: false })
-      cy.contains('Edit').click()
-      cy.contains('Delete Dataverse').click()
-      cy.get('.ui-dialog-content button[type="submit"]:visible').click()
-    })
+      // cy.visit('/dataverse/testDv', { failOnStatusCode: false })
+      // cy.contains('Edit').click()
+      // cy.contains('Delete Dataverse').click()
+      // cy.get('.ui-dialog-content button[type="submit"]:visible').click()
+    // })
   })
 
   beforeEach(function() {
     login(cy)
   })
 
-  it('can be created with minimal changes', function() {
+  it('can be created with minimal customization', function() {
     cy.visit('/dataverse.xhtml?ownerId=1')
 
     // setup aliases for used fields
@@ -32,7 +32,7 @@ describe('Dataverse', function() {
     cy.get('input[id="dataverseForm:facetsRoot"]').as('facetsRoot')
 
     // assert that there are no alerts in the initial state
-    cy.get('.alert').should('not.exist')
+    cy.get('.alert:not(.alert-success)').should('not.exist')
     cy.get('.ui-message-error-detail').should('not.exist')
 
     // assert that the initial values have been set correctly
@@ -49,7 +49,7 @@ describe('Dataverse', function() {
 
     // expect validation to fail for some key fields
     cy.get('.alert').should('exist')
-    cy.get('.ui-message-error-detail').should('to.have.length', 2)
+    cy.get('.ui-message-error-detail').should('have.length', 2)
 
     // fill in the remaining forms
     cy.get('@identifier').type('testDv')
@@ -63,7 +63,25 @@ describe('Dataverse', function() {
     cy.get('.dataverseHeaderCell').should('contain', 'Unpublished')
   })
 
-  it('allows datasets to be created', function() {
+  it.only('allows datasets to be created', function() {
     cy.visit('/dataverse/testDv')
+
+    // click through to the new dataset page
+    // TODO: this should all be tested in a different spec
+    cy.contains('Add Data').click()
+    cy.contains('New Dataset').click()
+
+    // fill in the necessary fields for the simplest new dataset
+    cy.get('.panel-body').within(() => {
+      cy.get('> :nth-child(1) input[type="text"]').type('Some dataset')
+      cy.get('> :nth-child(4) textarea').type('It is about some data')
+      cy.get('> :nth-child(5) tr:nth-child(5) div.ui-chkbox-box').click()
+    })
+
+    // save the new dataset
+    cy.contains('Save Dataset').click()
+
+    // ensure that we have been redirected to the correct dataverse page
+    cy.url().should('contain', '/dataset.xhtml')
   })
 })
