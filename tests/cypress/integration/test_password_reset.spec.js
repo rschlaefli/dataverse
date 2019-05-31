47a59154a3e13af27537f3c233dbe97638f9a985
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-05-31 19:37:54 +0200

    Cases for pw reset

--- tests/cypress/integration/test_password_reset.spec.js
+++ tests/cypress/integration/test_password_reset.spec.js
@@ -1,9 +1,21 @@
 describe('Password Reset', function() {
-  it('allows the user to request a password', function() {
+  it('allows a valid user to request its password', function() {
     cy.visit('/passwordreset.xhtml')
 
     cy.get('input[name="passwordReset:email"]').type(
       'dataverse@mailinator.com{enter}'
     )
+
+    cy.get('.alert').should('contain', 'Password Reset Initiated')
+  })
+
+  it('does not treat invalid users differently', function() {
+    cy.visit('/passwordreset.xhtml')
+
+    cy.get('input[name="passwordReset:email"]').type(
+      'invalid_user@gmail.com{enter}'
+    )
+
+    cy.get('.alert').should('contain', 'Password Reset Initiated')
   })
 })
